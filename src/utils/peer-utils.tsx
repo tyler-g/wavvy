4cb5f7b411957062dea6a511594d25460edbcec2
Tyler G <mail@tyler-g.com>
2024-09-01 14:28:50 -0400

    remove superjson pkg - doesn't work for syncing all complex instances

--- src/utils/peer-utils.tsx
+++ src/utils/peer-utils.tsx
@@ -1,11 +1,10 @@
-import superjson from 'superjson';
-
 import usePeerStore from '../stores/Peer';
 import useMixerStore, { MixerState, TrackState } from '../stores/Mixer';
 
 export const sendCmdToAllRemotePeers = (cmd: string, data: any) => {
   // send command to all connected remote peers
   const remotePeers = usePeerStore.getState().remote;
+  if (!remotePeers.length) return;
   console.log('sendCmdToAllRemotePeers', cmd, data, remotePeers);
   remotePeers.forEach((peer) => {
     console.log('send removeTrack to remote peer:', peer);
@@ -17,12 +16,8 @@ export const sendCmdToAllRemotePeers = (cmd: string, data: any) => {
 };
 
 export const sendCurrentStateToAllRemotePeers = () => {
-  // send the entire state to all connected remote peers
   const remotePeers = usePeerStore.getState().remote;
-
   const currentMixerActionHistory = useMixerStore.getState().history;
-
-  //const syncableState = transformMixerStateSyncable(currentMixerTracksState);
 
   console.log(
     'sendCmdToAllRemotePeers',
@@ -36,19 +31,4 @@ export const sendCurrentStateToAllRemotePeers = () => {
       data: currentMixerActionHistory,
     });
   });
-};
-
-interface MixterStateSyncable {
-  tracks: number[];
-}
-/* 
-  MixerState cannot be immediately serialized because it contains nested prototypes, fns etc.
-  transformMixerStateSyncable will take only the necessary data and make an object out of it
-*/
-export const transformMixerStateSyncable = (
-  state: MixerState
-): MixterStateSyncable => {
-  return {
-    tracks: state.tracks.map((track) => track.id),
-  };
 };
